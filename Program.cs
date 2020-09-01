37b6ed2a7076c76fce524be81f0e69f1011652e3
apaige161 <apaige161@gmail.com>
2020-08-31 21:22:00 -0400

    added more regex examples

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using static System.Console;
 using System.Xml.Linq;
-using SharedLibrary;
 using System.Text.RegularExpressions;
 
 namespace AssembliesAndNamespaces
@@ -10,6 +9,8 @@ namespace AssembliesAndNamespaces
     {
         public static bool IsValidXmlTag(this string input)
         {
+            //(?<word>\w+)	Match one or more word characters up to a word boundary. Name this captured group word.
+            //\s+	Match one or more white-space characters.
             return Regex.IsMatch(input, @"^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)$");
         }
 
@@ -30,8 +31,7 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
-            var doc = new XDocument();
-
+            /*
             Write("Enter a color value in hex:");
             string hex = ReadLine();
             WriteLine($"Is {hex} a valid color value?: {hex.IsValidHex()}");
@@ -43,6 +43,38 @@ namespace AssembliesAndNamespaces
             Write("Enter a password: ");
             string password = ReadLine();
             WriteLine($"Is {password} a valid password?: {password.IsValidPassword()}");
+            */
+
+            Write("Enter your age: ");
+            string ageInput = ReadLine();
+
+            // ^ means start of input
+            // \d looks for a single digit
+            // + looks for one or more of whatever is in front of it
+            // $ end of input
+            var ageChecker = new Regex(@"^\d+$");
+            if(ageChecker.IsMatch(ageInput))
+            {
+                WriteLine("Thank You, this is validated");
+            }
+            else
+            {
+                WriteLine($"Invalid age: {ageInput}");
+            }
+
+            //splitting a complex csv string
+            string films = "\"Monsters, Inc.\",\"I, Tonya\",\"Lock, Stock and Two Smoking Barrels\"";
+            var csv = new Regex("(?:^|,)(?=[^\"]|(\")?)\"?((?(1)[^\"]*|[^,\"]*))\"?(?=,|$)");
+            MatchCollection filmsSmart = csv.Matches(films);
+            WriteLine("Smart attempt at splitting: ");
+            
+            foreach(Match film in filmsSmart)
+            {
+                WriteLine(film.Groups[2].Value);
+            }
+
+
+
         }
     }
 }
